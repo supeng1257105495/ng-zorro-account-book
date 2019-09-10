9c040efb1ffa1ed19220327b57afa7a3ae3fb460
supeng <1257105495@qq.com>
2019-09-10 16:25:37 +0800

    多语言的初级支持方案 前端静态数据 多语言支持

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { slideInAnimation } from './animations';
 import { RouterOutlet } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -8,12 +9,30 @@ import { RouterOutlet } from '@angular/router';
   styleUrls: ['./app.component.css'],
   animations: [slideInAnimation]
 })
-export class AppComponent {
-  // @HostBinding('@routeAnimations') state;
-
+export class AppComponent implements OnInit {
   isCollapsed = false;
+  menuList = [{ label: '账单', icon: 'book', link: '/record' }, { label: '图表', icon: 'bar-chart', link: '/chart' }];
 
-  menu = [{ label: '账单', icon: 'book', link: '/record' }, { label: '图表', icon: 'bar-chart', link: '/chart' }];
+  constructor(public translate: TranslateService) {}
+
+  async ngOnInit() {
+    // 语言初始化(若未设置语言, 则取浏览器语言)
+    const currentLanguage = (await localStorage.getItem('currentLanguage')) || this.translate.getBrowserCultureLang();
+
+    // 当在assets/i18n中找不到对应的语言翻译时，使用'zh-CN'作为默认语言
+    this.translate.setDefaultLang('zh-CN');
+    this.translate.use(currentLanguage);
+
+    // 记录当前设置的语言
+    localStorage.setItem('currentLanguage', currentLanguage);
+  }
+
+  // 设置语言
+  selectLanguage(lang: string) {
+    this.translate.use(lang);
+    // 更新当前记录的语言
+    localStorage.setItem('currentLanguage', lang);
+  }
 
   prepareRoute(outlet: RouterOutlet) {
     return outlet.isActivated ? outlet.activatedRoute : '';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { NgZorroAntdModule, NZ_I18N, zh_CN } from 'ng-zorro-antd';
 import { AppComponent } from './app.component';
+// 国际化
+import { TranslateModule, TranslateLoader, TranslateService } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function HttpLoaderFactory(httpClient: HttpClient) {
+  return new TranslateHttpLoader(httpClient);
+}
 
 // 配置 angular i18n
 import { registerLocaleData } from '@angular/common';
@@ -29,7 +36,15 @@ import { NgxEchartsModule } from 'ngx-echarts';
     AppRoutingModule,
     IconsProviderModule,
     // 外部引入模块
-    NgxEchartsModule
+    NgxEchartsModule,
+    // 国际化
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   bootstrap: [AppComponent],
   // 配置 ng-zorro-antd 国际化（文案 及 日期）
