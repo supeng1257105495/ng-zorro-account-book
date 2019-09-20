47d2fa5b5cc23c5a52ee9881569daa3b3261c940
supeng <1257105495@qq.com>
2019-09-20 09:20:42 +0800

    缓存

--- src/app/pages/record/detail-form/detail-form.component.ts
+++ src/app/pages/record/detail-form/detail-form.component.ts
@@ -8,13 +8,34 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class DetailFormComponent implements OnInit {
   validateForm: FormGroup;
+
+  submitForm(): void {
+    for (const i in this.validateForm.controls) {
+      if (this.validateForm.controls.hasOwnProperty(i)) {
+        this.validateForm.controls[i].markAsDirty();
+        this.validateForm.controls[i].updateValueAndValidity();
+      }
+    }
+  }
+
   constructor(private fb: FormBuilder) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.validateForm = this.fb.group({
       userName: [null, [Validators.required]],
       password: [null, [Validators.required]],
       remember: [true]
     });
+  }
+
+  resetForm(e: MouseEvent): void {
+    e.preventDefault();
+    this.validateForm.reset();
+    for (const key in this.validateForm.controls) {
+      if (this.validateForm.controls.hasOwnProperty(key)) {
+        this.validateForm.controls[key].markAsPristine();
+        this.validateForm.controls[key].updateValueAndValidity();
+      }
+    }
   }
 }
