5a2bb47a45f04760ac446351a076a0ea708688e1
supeng <1257105495@qq.com>
2019-09-23 15:19:45 +0800

    fix-form

--- src/app/pages/record/detail-form/detail-form.component.ts
+++ src/app/pages/record/detail-form/detail-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-detail-form',
